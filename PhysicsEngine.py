382dd25a66165b014146334359ef229eddd57c89
JACKYCHAN184 <jackrochford@hotmail.co.uk>
2022-04-08 19:08:02 +0100

    dont worry about it

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,7 +1,9 @@
+from cmath import acos, asin, cos, sin
 import math
+from time import time
 
 class Particle:
-    def __init__(self,mass,velocity,x,y,acceleration):
+    def __init__(self,mass,velocity,x,y,acceleration,angle,time):
         self.mass =  mass
         self.velocity = velocity
         self.x = x
@@ -9,5 +11,17 @@ class Particle:
         self.speed = abs(self.velocity)
         self.acceleration = acceleration
         self.momentum = self.mass * self.speed
+        self.angle = angle
+
+
+    def jump(self, velocity, x, y, speed, acceleration, momentum,angle):
+        total_time = 1.5
+        total_frames = 90
+        frame_time = total_time/total_frames
+        for i in range(1,total_frames):
+            time = frame_time*i
+            self.x = cos(self.angle)*(self.velocity*time)+0.5(acceleration*pow(time,2))
+            self.y = sin(self.angle)*(self.velocity*time)+0.5(acceleration*pow(time,2))
+
 
     
