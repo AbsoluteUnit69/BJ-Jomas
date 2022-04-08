33503a6b2dc53434deffe3a37fa6ca5b95013d00
AbsoluteUnit69 <freddietboyd@outlook.com>
2022-04-08 13:04:16 +0100

    Map maker with grid

--- Map.py
+++ Map.py
@@ -14,12 +14,28 @@ class Block(pygame.sprite.Sprite):
 class Map_maker():
     def __init__(self):
         self.screen = pygame.display
-        self.screen_rect = self.screen.set_mode((700, 500))
+        self.screen_rect = self.screen.set_mode((960, 540))
         self.blocks = []
-        positions = [[(0, 480), 700, 20], [(200, 250), 150, 20]]
-        for pos in positions:
-            self.blocks.append(Block((128, 0, 0), pos[1], pos[2], (pos[0])))
+        self.points = []
+        for row in range(0, 960, 12):
+            for column in range(0, 540, 12):
+                self.points.append((row, column))
+        self.line_pos_y = []
+        for i in range(0, 960, 12):
+            self.line_pos_y.append((i, 0))
+        self.line_pos_x = []
+        for i in range(0, 540, 12):
+            self.line_pos_x.append((0, i))
         self.start_loop()
+
+    def get_closest(self, pos):
+        x_twelves = pos[0]//12
+        if (pos[0]%12) >= 6:
+            x_twelves += 1
+        y_twelves = pos[1]//12
+        if (pos[1]%12) >= 6:
+            y_twelves += 1
+        return ((x_twelves*12),(y_twelves*12))
 
     def start_loop(self):
         run = True
@@ -27,8 +43,6 @@ class Map_maker():
         while run:
 
             self.screen_rect.fill((100, 100, 100))
-
-            pos2 = pygame.mouse.get_pos()
 
             for b in self.blocks:
                 b.display(self.screen_rect)
@@ -38,12 +52,14 @@ class Map_maker():
                     run = False
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pos1 = pygame.mouse.get_pos()
+                    pos1 = self.get_closest(pos1)
                 if event.type == pygame.MOUSEBUTTONUP:
                     if width*height > 100:
                         self.blocks.append(Block((128, 0, 0), width, height, top_left))
                     pos1 = None
 
             pos2 = pygame.mouse.get_pos()
+            pos2 = self.get_closest(pos2)
 
             if pos1 is not None:
                 top_left = (min(pos1[0], pos2[0]), min(pos1[1], pos2[1]))
@@ -52,6 +68,12 @@ class Map_maker():
                 height = abs(top_left[1] - bottom_left[1])
                 new_rect = pygame.Rect(top_left[0], top_left[1], width, height)
                 pygame.draw.rect(self.screen_rect, (0, 100, 100), new_rect)
+
+            for pos in self.line_pos_y:
+                pygame.draw.line(self.screen_rect, (0, 0, 0), pos, (pos[0], 540))
+            
+            for pos in self.line_pos_x:
+                pygame.draw.line(self.screen_rect, (0, 0, 0), pos, (960, pos[1]))
 
             self.screen.update()
 
