9228d768d9d1b2e66fb3fbb2448f494ff5100527
Joshiling <mail@joshuarawlings.com>
2022-04-09 22:31:34 +0100

    classes added

--- MainGame.py
+++ MainGame.py
@@ -1,4 +1,11 @@
-'''
+class MainGame():
+    def __init__(self):
+        pass
+
+    def mainMenu(self):
+        print("main menu")
+    
+    def mainLoop(self):
+        print("rounds and stuff")
 
 
-'''
---
+++ Rounds.py
@@ -0,0 +1,8 @@
+class Round():
+    def __init__(self):
+        pass
+    def mainLoop(self):
+        print("player stuff")
+        print("move the map")
+        #handle groups of players and ghosts
+        
