10978631bc01c459bca2c0374cd7025aa3d6f8ff
JACKYCHAN184 <jackrochford@hotmail.co.uk>
2022-04-13 22:21:53 +0100

    New jump ish

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,15 +1,19 @@
 from cmath import cos, sin
 import math
+from time import time
 
 class Particle:
     def __init__(self,mass,velocity,x,y,acceleration,angle):
         self.mass =  mass
+        #self.velocity = velocity
         self.velocity = velocity
-        self.x = x
-        self.y = y
+        self.x = 0
+        self.y = 0
+        #self.speed = abs(self.velocity)
         self.speed = abs(self.velocity)
-        self.acceleration = 9.81
+        self.acceleration = acceleration
         self.momentum = self.mass * self.speed
+        #self.angle = angle
         self.angle = angle
         self.displacement = 0
 
@@ -32,10 +36,9 @@ Even though it looks shit it should probably work after ive fixed some small err
         x_points = []
         y_points = []
         y_spd = self.speed
-
         while y_spd != 0:
-            x_pos = final_x_vel - initial_x_vel / 2*self.acceleration
-            y_pos = final_y_vel - initial_y_vel / 2*self.acceleration
+            x_pos = pow(final_x_vel,2) - pow(initial_x_vel,2) / 2*self.acceleration
+            y_pos = pow(final_y_vel,2) - pow(initial_y_vel,2) / 2*self.acceleration
             x_points.append(x_pos)
             y_points.append(y_pos)
             final_x_vel = initial_x_vel
@@ -57,6 +60,46 @@ Even though it looks shit it should probably work after ive fixed some small err
             self.y = original_y + y_pos
             player_pos = (math.trunc(self.x),math.trunc(self.y))
             print(player_pos)
+
+    def newJump(self):
+        original_y = self.y
+        original_x = self.x
+        total_jump_time = 1.5 #seconds
+        frames = 90
+        x_speed = self.velocity*cos(self.angle)
+        y_initial_velocity = self.speed*sin(self.angle)
+        time_per_frame = total_jump_time / frames
+        for i in range(0,frames):
+            y_displacement = y_initial_velocity*time + self.acceleration*time**2
+            x_distance = x_speed*time
+            y_final_velocity = y_initial_velocity + self.acceleration*time
+            y_initial_velocity = y_final_velocity
+            time = time + time_per_frame
+            self.y = math.trunc(original_y + y_displacement)
+            self.x = math.trunc(original_x + x_distance)
+            player_pos = (self.x,self.y)
+
+    def newerJump(self):
+        original_y = self.y
+        original_x = self.x
+        x_speed = self.velocity*cos(self.angle)
+        y_initial_velocity = self.speed*sin(self.angle)
+        time_frame = 0.1
+        time = 0
+        not_colliding = True
+        while not_colliding:
+            y_displacement = y_initial_velocity*time + self.acceleration*time**2
+            x_distance = x_speed*time
+            y_final_velocity = y_initial_velocity + self.acceleration*time
+            y_initial_velocity = y_final_velocity
+            time = time + time_frame
+            self.y = math.trunc(original_y + y_displacement)
+            self.x = math.trunc(original_x + x_distance)
+            player_pos = (self.x,self.y)
+            if player_pos == 69:    # need some thing here so if he lands on/hits something it stops but otherwise keeps going e.g. if the player over shoots or undershoots a platform
+                not_colliding = False
+            
+
 
 
 
